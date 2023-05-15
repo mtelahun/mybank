c7737668ee383272cf1fce810e363a5f773ea8dd
Michael Telahun <mtm@trevi.et>
2023-05-15 23:26:03 +0300

    Build logic and test domain of our BankAccount Aggregate
    Add:
      - Commands
      - Domain Events
      - Error
      - Service
      - Aggregate
      - Domain tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,209 @@
+use std::fmt::Display;
+
+use async_trait::async_trait;
+use cqrs_es::{Aggregate, DomainEvent};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+pub enum BankAccountCommand {
+    OpenAccount { account_id: String },
+    DepositMoney { amount: f64 },
+    WithdrawMoney { amount: f64 },
+    WriteCheck { check_number: String, amount: f64 },
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum BankAccountEvent {
+    AccountOpened {
+        account_id: String,
+    },
+    CustomerDepositedMoney {
+        amount: f64,
+        balance: f64,
+    },
+    CustomerWithdrewCash {
+        amount: f64,
+        balance: f64,
+    },
+    CustomerWroteCheck {
+        check_number: String,
+        amount: f64,
+        balance: f64,
+    },
+}
+
+impl DomainEvent for BankAccountEvent {
+    fn event_type(&self) -> String {
+        let event_type = match self {
+            BankAccountEvent::AccountOpened { .. } => "AccountOpened",
+            BankAccountEvent::CustomerDepositedMoney { .. } => "CustomerDepositedMoney",
+            BankAccountEvent::CustomerWithdrewCash { .. } => "CustomerWithdrewCash",
+            BankAccountEvent::CustomerWroteCheck { .. } => "CustomerWroteCheck",
+        };
+
+        event_type.to_string()
+    }
+
+    fn event_version(&self) -> String {
+        "1.0".to_string()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BankAccountError(String);
+
+impl Display for BankAccountError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for BankAccountError {}
+
+impl From<&str> for BankAccountError {
+    fn from(value: &str) -> Self {
+        BankAccountError(value.to_string())
+    }
+}
+
+pub struct BankAccountServices;
+
+impl BankAccountServices {
+    async fn _atm_withdrawal(&self, _atm_id: &str, _amount: f64) -> Result<(), AtmError> {
+        Ok(())
+    }
+
+    async fn _validate_check(&self, _account: &str, _check: &str) -> Result<(), CheckingError> {
+        Ok(())
+    }
+}
+
+pub struct AtmError;
+pub struct CheckingError;
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct BankAccount {
+    opened: bool,
+    balance: f64,
+}
+
+#[async_trait]
+impl Aggregate for BankAccount {
+    type Command = BankAccountCommand;
+    type Event = BankAccountEvent;
+    type Error = BankAccountError;
+    type Services = BankAccountServices;
+
+    fn aggregate_type() -> String {
+        "Account".to_string()
+    }
+
+    async fn handle(
+        &self,
+        command: Self::Command,
+        _services: &Self::Services,
+    ) -> Result<Vec<Self::Event>, Self::Error> {
+        match command {
+            BankAccountCommand::DepositMoney { amount } => {
+                let balance = self.balance + amount;
+                Ok(vec![BankAccountEvent::CustomerDepositedMoney {
+                    amount,
+                    balance,
+                }])
+            }
+            BankAccountCommand::WithdrawMoney { amount } => {
+                let balance = self.balance - amount;
+                if balance < 0_f64 {
+                    return Err("funds not available".into());
+                }
+                Ok(vec![BankAccountEvent::CustomerWithdrewCash {
+                    amount,
+                    balance,
+                }])
+            }
+            _ => Ok(vec![]),
+        }
+    }
+
+    fn apply(&mut self, event: Self::Event) {
+        match event {
+            BankAccountEvent::AccountOpened { .. } => self.opened = true,
+            BankAccountEvent::CustomerDepositedMoney { amount: _, balance } => {
+                self.balance = balance
+            }
+            BankAccountEvent::CustomerWithdrewCash { amount: _, balance } => self.balance = balance,
+            BankAccountEvent::CustomerWroteCheck {
+                check_number: _,
+                amount: _,
+                balance,
+            } => self.balance = balance,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod aggregate_tests {
+    use super::*;
+    use cqrs_es::test::TestFramework;
+
+    type AccountTestFramework = TestFramework<BankAccount>;
+
+    #[test]
+    fn test_deposit_money() {
+        let expected = BankAccountEvent::CustomerDepositedMoney {
+            amount: 200.0,
+            balance: 200.0,
+        };
+
+        AccountTestFramework::with(BankAccountServices)
+            .given_no_previous_events()
+            .when(BankAccountCommand::DepositMoney { amount: 200.0 })
+            .then_expect_events(vec![expected]);
+    }
+
+    #[test]
+    fn test_deposit_money_with_balance() {
+        let previous = BankAccountEvent::CustomerDepositedMoney {
+            amount: 200.0,
+            balance: 200.0,
+        };
+        let expected = BankAccountEvent::CustomerDepositedMoney {
+            amount: 200.0,
+            balance: 400.0,
+        };
+
+        AccountTestFramework::with(BankAccountServices)
+            .given(vec![previous])
+            .when(BankAccountCommand::DepositMoney { amount: 200.0 })
+            .then_expect_events(vec![expected])
+    }
+
+    #[test]
+    fn test_withdraw_money() {
+        let previous = BankAccountEvent::CustomerDepositedMoney {
+            amount: 200.0,
+            balance: 200.0,
+        };
+        let expected = BankAccountEvent::CustomerWithdrewCash {
+            amount: 100.0,
+            balance: 100.0,
+        };
+
+        AccountTestFramework::with(BankAccountServices)
+            .given(vec![previous])
+            .when(BankAccountCommand::WithdrawMoney { amount: 100.0 })
+            .then_expect_events(vec![expected])
+    }
+
+    #[test]
+    fn test_withdraw_money_funds_unavailable() {
+        AccountTestFramework::with(BankAccountServices)
+            .given_no_previous_events()
+            .when(BankAccountCommand::WithdrawMoney { amount: 200.0 })
+            .then_expect_error(BankAccountError("funds not available".to_string()))
+    }
 }
